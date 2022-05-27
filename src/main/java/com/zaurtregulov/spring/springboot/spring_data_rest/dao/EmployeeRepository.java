7607960fe0d556d6064763ce98d32dc64a68f5ae
Dmitry Shishkin <4144442@bk.ru>
2022-05-27 10:16:05 +0300

    Spring Data Rest. Spring Actuator

---
+++ src/main/java/com/zaurtregulov/spring/springboot/spring_data_rest/dao/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.zaurtregulov.spring.springboot.spring_data_rest.dao;
+
+import com.zaurtregulov.spring.springboot.spring_data_rest.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
